9cf3442263fd41bf2e6ca7cdd12e3878bf92ee95
Yogesh-Yogi-1 <yogeshyogiofficial@gmail.com>
2024-03-12 19:16:51 +0530

    Success

--- web.py
+++ web.py
@@ -1,26 +1,3 @@
-# EX01 Developing a Simple Webserver
-## Date:
-12/03/2024
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
 <html>
     <head>
 
@@ -29,67 +6,67 @@ Testing the webserver.
         <table border="5">
     
             <tr>
-	          <th>Rank</th>
+	      <th>Rank</th>
               <th>Company</th>
               <th>Revenue</th>
               <th>FY</th>
             </tr>
             <tr>
-	         <td>1</td>
+	     <td>1</td>
              <td>Microsoft</td>
              <td>$86.8</td>
              <td>2014</td>
             </tr>
             <tr>
-	          <td>2</td>
+	      <td>2</td>
               <td>Oracle</td>
               <td>$37.1</td>
               <td>2013</td>
             </tr>
-	        <tr>
-	          <td>3</td>
+	    <tr>
+	      <td>3</td>
               <td>SAP</td>
               <td>$20.9</td>
               <td>2013</td>
             </tr>
             <tr>
-	          <td>4</td>
+	      <td>4</td>
               <td>Symantec</td>
               <td>$6.8</td>
               <td>2013</td>
             </tr>
-	        <tr>
-	          <td>5</td>
+	    <tr>
+	      <td>5</td>
               <td>VMware</td>
               <td>$5.2</td>
               <td>2013</td>
             </tr>
-	        <tr>
-	          <td>6</td>
+	    <tr>
+	      <td>6</td>
               <td>CA Technologies</td>
               <td>$4.7</td>
               <td>2013</td>
             </tr>
     	    <tr>	
-	          <td>7</td>
+	      <td>7</td>
               <td>Adobe Systems</td>
               <td>$4.4</td>
               <td>2013</td>
             </tr>
-	        <tr>
-	          <td>8</td>
+	    <tr>
+	      <td>8</td>
               <td>Fiserv</td>
               <td>$4.5</td>
               <td>2013</td>
             </tr>
-	        <tr>
-	          <td>9</td>
+	    <tr>
+	      <td>9</td>
               <td>Intuit</td>
               <td>$4.2</td>
               <td>2013</td>
             </tr>
-	        <tr>
-	          <td>10</td>
+	    <tr>
+	      <td>10</td>
               <td>Amadeus IT Group</td>
               <td>$3.8</td>
               <td>2013</td>
@@ -99,9 +76,3 @@ Testing the webserver.
           
     </body>
 </html>
-
-## OUTPUT:
-![alt text](<Screenshot 2024-03-12 190612.png>)
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
